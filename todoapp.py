b5d19fa78405ae990134e97750ffc245fc145f53
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-11-04 17:47:17 -0400

    Assignment 11

--- todoapp.py
+++ todoapp.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+import re
 
 app = Flask(__name__)
 
-todo = ['Get Up', 'Brush Teeth', 'Wash Ass']
+todo = []
 
 # templates need to be in a folder called "templates"
 # The "templates" folder needs to be checked into the repo as well
@@ -16,21 +18,26 @@ def first_controller():
     return render_template('displaytodolist.html', todo = todo)
 
 
-@app.route('/morehello')
-def more_hello():
-    return 'Hello Everybody!!!!!'
+@app.route('/submit', methods=['POST'])
+def second_controller():
+    task = request.form['task']
+    email = request.form['email']
+    priority = request.form['priority']
 
+    if not re.search(r"(\.[a-z]{3}$|\.[a-z]{2}$)", email):
+        print("invalid email")
+        return redirect('/')
+    elif not re.search(r"(high|medium|low)", priority):
+        print("invalid priority")
+        return redirect('/')
+    else:
+        todo.append(task)
+        return redirect('/')
 
-@app.route('/hello/')
-@app.route('/hello/<username>')
-def helloname(username=None):
-    return render_template('hellotemplate.html', name=username)
-
-
-@app.route('/helloaction', methods=['POST', 'GET'])
-def hellaction():
-    if request.method == 'POST':
-        return render_template('hellotemplate.html', name=request.form['username'])
+@app.route('/clear', methods=['POST'])
+def third_controller():
+    todo.clear()
+    return redirect('/')
 
 
 if __name__ == "__main__":
